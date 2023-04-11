9dbf18931b58eea089e71010294cf744679766da
Pavel <rut100500@yandex.ru>
2023-04-11 22:40:14 +0300

    дескрипторы 1_1 c age

--- home01.py
+++ home01.py
@@ -11,22 +11,32 @@ class NonNegative:
     # #     # owner - владелец атрибута - <class '__main__.Worker'>
     # #     # my_attr - имя атрибута владельца - hours, rate
     #     self.my_attr = my_attr
+class Defence_age:
+    def __init__(self, my_attr):
+        self.my_attr = my_attr
+
+    def __set__(self, var, value):
+        if value < 18:
+            raise ValueError("Возраст может быть менее 18 лет")
+        var.__dict__[self.my_attr] = value
 
 class Worker:
     # имя атрибута, который делаем дескриптором, в конструктор не передаем
     hours = NonNegative("hours")
     rate = NonNegative("rate")
+    age = Defence_age("age")
 
-    def __init__(self, name, surname, hours, rate):
+    def __init__(self, name, surname, hours, rate, age):
         self.name = name
         self.surname = surname
         self.hours = hours
         self.rate = rate
+        self.age = age
 
     def total_profit(self):
         return self.hours * self.rate
 
-s1 = Worker('Иван', 'Иванов', -10, 100)
+s1 = Worker('Иван', 'Иванов', 10, 100, 100)
 print(s1.total_profit())
 
 # OBJ.hours = 10
